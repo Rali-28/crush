bb37a83fe6a692b971e1f8e62f52ac59e6e6c3c0
Rali <113418936+Rali-28@users.noreply.github.com>
2025-11-20 01:28:16 +0800

    Add image to question

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import meme from "../images/meme.png";
 
-const Question = ({onYes}) => {
+const Question = ({ onYes }) => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
@@ -13,8 +14,8 @@ const Question = ({onYes}) => {
     const maxX = containerWidth - buttonWidth;
     const maxY = containerHeight - buttonHeight;
 
-    setX((Math.random() * maxX) - 125);
-    setY((Math.random() * maxY) - 200);
+    setX(Math.random() * maxX - 125);
+    setY(Math.random() * maxY - 200);
     console.log(x);
     console.log(y);
   };
@@ -22,6 +23,7 @@ const Question = ({onYes}) => {
   return (
     <div className="flex items-center justify-center w-full h-dvh">
       <div className="flex flex-col items-center justify-center w-full h-screen overflow-hidden">
+        <img src={meme.src} alt="" />
         <h2>Crush mo ba ako?</h2>
         <div className="relative w-full text-center h-fit">
           <button onClick={onYes}>OO</button>
