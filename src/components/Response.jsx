24c4a71790f591f3e73c9719f6686c8297607cbc
Rali <113418936+Rali-28@users.noreply.github.com>
2025-11-20 02:43:38 +0800

    Finalize response

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import gif from "../images/binified-blooms-hub-bbh.gif";
 
 const Response = () => {
   return (
-    <div>crush mo pala ako ah</div>
-  )
-}
+    <div className="flex flex-col gap-1 items-center justify-center w-full h-dvh">
+      <img src={gif.src} alt="Malupiton Bini" />
+      <h2 className="text-3xl">Sabi na eh crush mo ko</h2>
+    </div>
+  );
+};
 
-export default Response
+export default Response;
